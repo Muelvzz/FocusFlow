c8d3239df650dc2fba68c16a2927b67e76c21d35
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-10-16 20:17:59 +0800

    todolist section v.2

--- frontend/src/sections/TodoList.jsx
+++ frontend/src/sections/TodoList.jsx
@@ -1,8 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../sections-css/todolist.css'
 
 export default function TodoList() {
     const [addButton, setAddButton] = useState(false)
+    const [userTask, setUserTask] = useState("")
+    const [showEdit, setShowEdit] = useState(false)
+    const [editingIndex, setEditingIndex] = useState(null)
+    const [editingValue, setEditingValue] = useState("")
+    const [taskList, setTaskList] = useState(() => {
+        const saved = localStorage.getItem("userTask");
+        return saved ? JSON.parse(saved) : []
+    })
+
+    const handleChange = (e) => {
+        setUserTask(e.target.value)
+    }
+
+    const handleSubmit = () => {
+        if (userTask.trim() !== "") {
+            setTaskList([...taskList, userTask])
+            console.log(taskList)
+        }
+        setUserTask("")
+    }
+
+    const handleDelete = (index) => {
+        const updatedTaskList = taskList.filter((_, i) => i !== index)
+        setTaskList(updatedTaskList)
+    }
+
+    const handleUpdate = () => {
+        if (editingValue.trim() !== "") {
+            taskList[editingIndex] = editingValue
+            setTaskList(taskList)
+            setEditingValue("")
+            setShowEdit(false)
+        }
+    }
+
+    useEffect(() => {
+        localStorage.setItem("userTask", JSON.stringify(taskList))
+    }, [taskList])
 
     return (
         <div className="todolist-container">
@@ -26,13 +64,47 @@ export default function TodoList() {
                         type="text"
                         placeholder="Enter your task..."
                         className="todolist-input"
+                        value={ userTask }
+                        onChange={ handleChange }
                     />
-                    <button className="add-btn">Add</button>
+                    <button className="add-btn" onClick={ () => handleSubmit() }>Add</button>
                 </div>
             )}
 
             <div className="todolist-content">
-                <p className="empty-state">No tasks yet. Add your first one!</p>
+                <ul className="todolist-list">
+                {taskList.length > 0 ? (
+                    taskList.map((entry, index) => (
+                    <li className="task-entry" key={index}>
+                        <span>
+                            <button
+                                className="task-edit-btn"
+                                onClick={() => {setShowEdit(!showEdit); setEditingIndex(index)}}
+                            >✏️</button>
+                            {entry}
+                        </span>
+                        <button
+                        className="task-del-btn"
+                        onClick={() => handleDelete(index)}
+                        >❌</button>
+                    </li>
+                    ))
+                ) : (
+                    <p className="empty-state">No tasks yet. Add your first one!</p>
+                )}
+
+                {showEdit && (
+                    <div className="todolist-input-container">
+                        <input
+                            type="text"
+                            className="todolist-input"
+                            value={ editingValue }
+                            onChange={ (e) => setEditingValue(e.target.value) }
+                        />
+                        <button className="add-btn" onClick={handleUpdate} type='submit'>Add</button>
+                    </div>
+                )}
+                </ul>
             </div>
 
             <div className="todolist-footer"></div>
