0e55a08ae6f00fc63e877bedd9e39000fc721615
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-10-20 22:19:59 +0800

    add motivational quotes on the header

---
+++ frontend/src/components/quotes.js
@@ -0,0 +1,26 @@
+export function quotes() {
+
+    const quotes = [
+        "If something you're doing, you hated it - why don't you try to do it differently?",
+        "In order to start something in life, you have to quit on one thing.",
+        "Any of these time managements would work if you could stick to them.",
+        "Displine is a system, and not a feeling.",
+        "Change happens when a greater pain is added.",
+        "Rock bottom is a remarkable event of transformation",
+        "Are you feeling you're going somewhere?",
+        "Find 1% improvement of what you're doing.",
+        "Big winners pay for so many experiments.",
+        "We learn skills through failure.",
+        "It is far better to understand why you failed than to be ignorant of why you succeeded.",
+        "If you don't learn, you will be stupid.",
+        "Systems get you move forward.",
+        "The most valuable that you have is energy'",
+        "If you don't produce anything, you won't thrive no matter how skilled or intelligent you are.",
+        "To learn require intense concentration.",
+        "We tend to place a lot of emphasis on our circumstance, as if it determines how we feel.",
+        "Our brains construct a worldview based on what we pay attention to.",
+        "Who you are, what you think, and do what you love is the sum of what you focus on.",
+    ]
+
+    return quotes
+}
--- frontend/src/sections/Header.jsx
+++ frontend/src/sections/Header.jsx
@@ -1,10 +1,25 @@
 import '../sections-css/header-footer.css'
+import { quotes } from '../components/quotes'
+import { useEffect, useState } from 'react'
 
 export default function Header({ isDark, setIsDark }) {
+
+    const [quote, setQuote] = useState('')
+
+    useEffect(() => {
+        const allQuote = quotes()
+        const quoteSize = allQuote.length
+        const quoteIndex = Math.floor(Math.random() * quoteSize)
+        setQuote(allQuote[quoteIndex])
+    }, [])
+
     return (
         <>
             <nav className={ isDark ? "dark-nav" : "light-nav" }>
                 <div className="left-nav">FocusFlow</div>
+                <div className="middle-nav">
+                    <i>"{ quote }"</i>
+                </div>
                 <div className="right-nav">
                     <ul>
                         <li>
